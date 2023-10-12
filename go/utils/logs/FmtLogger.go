8348a1f69e4396e0791d27e0ac6c45432c9dd8cf
Sharon Aicler <saichler@gmail.com>
2023-10-12 06:39:34 -0700

    Refactor logging

--- go/utils/logs/FmtLogger.go
+++ go/utils/logs/FmtLogger.go
@@ -3,7 +3,6 @@ package logs
 import (
 	"errors"
 	"fmt"
-	"github.com/saichler/my.simple/go/utils/strng"
 )
 
 type FmtLogger struct {
@@ -15,77 +14,21 @@ func NewFmtLogger() *FmtLogger {
 }
 
 func (logger FmtLogger) Trace(any interface{}, anys ...interface{}) {
-	str := strng.New("<Trace   > ")
-	str.TypesPrefix = false
-	str.AddSpaceWhenAdding = true
-	s, _ := str.StringOf(any)
-	str.Add(s)
-	if anys != nil {
-		for _, a := range anys {
-			s, _ = str.StringOf(a)
-			str.Add(s)
-		}
-	}
-	fmt.Println(str.String())
+	fmt.Println(TraceToString(any, anys))
 }
 
 func (logger FmtLogger) Debug(any interface{}, anys ...interface{}) {
-	str := strng.New("< Debug   > ")
-	str.TypesPrefix = false
-	str.AddSpaceWhenAdding = true
-	s, _ := str.StringOf(any)
-	str.Add(s)
-	if anys != nil {
-		for _, a := range anys {
-			s, _ = str.StringOf(a)
-			str.Add(s)
-		}
-	}
-	fmt.Println(str.String())
+	fmt.Println(DebugToString(any, anys))
 }
 
 func (logger FmtLogger) Info(any interface{}, anys ...interface{}) {
-	str := strng.New("<  Info    > ")
-	str.TypesPrefix = false
-	str.AddSpaceWhenAdding = true
-	s, _ := str.StringOf(any)
-	str.Add(s)
-	if anys != nil {
-		for _, a := range anys {
-			s, _ = str.StringOf(a)
-			str.Add(s)
-		}
-	}
-	fmt.Println(str.String())
+	fmt.Println(InfoToString(any, anys))
 }
 
 func (logger FmtLogger) Warning(any interface{}, anys ...interface{}) {
-	str := strng.New("<   Warning > ")
-	str.TypesPrefix = false
-	str.AddSpaceWhenAdding = true
-	s, _ := str.StringOf(any)
-	str.Add(s)
-	if anys != nil {
-		for _, a := range anys {
-			s, _ = str.StringOf(a)
-			str.Add(s)
-		}
-	}
-	fmt.Println(str.String())
+	fmt.Println(WarningToString(any, anys))
 }
 
 func (logger FmtLogger) Error(any interface{}, anys ...interface{}) error {
-	str := strng.New("<      Error > ")
-	str.TypesPrefix = false
-	str.AddSpaceWhenAdding = true
-	s, _ := str.StringOf(any)
-	str.Add(s)
-	if anys != nil {
-		for _, a := range anys {
-			s, _ = str.StringOf(a)
-			str.Add(s)
-		}
-	}
-	fmt.Println(str.String())
-	return errors.New(str.String())
+	return errors.New(ErrorToString(any, anys))
 }
